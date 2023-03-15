0d131031921384e3d6ddb874128284052ce3fb55
Dharshan Prasath H <58679563+Dharshan2000@users.noreply.github.com>
2023-03-15 11:06:13 +0530

    Add files via upload

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(String[] args) 
+        {
+            Console.Write("Reveresed letters : ");
+            string reverseFirstName=HellowWorld("Dharshan");
+            string reverseLastName=HellowWorld("Prasath");
+            string reverseCity=HellowWorld("Kotagiri");
+            //Console.Write(String.Format("{0} {1} {2}", reverseFirstName, reverseLastName, reverseCity);
+           //Console.ReadLine();
+           DisplayResult(reverseFirstName,reverseLastName,reverseCity);
+            Console.ReadLine();
+        }
+
+        private static string HellowWorld(string message)
+        {
+            char[] messageArray = message.ToArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+        }
+        private static void DisplayResult(string reversedFirstName,string reversedLastName, string reversedCity)
+        {
+            Console.Write(String.Format("{0} {1} {2}", reverseFirstName, reverseLastName, reverseCity);
+            Console.ReadLine();
+        }
+    }
+}
