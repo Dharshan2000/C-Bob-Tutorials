317db50a1ffb900d837d1a72fa130d2a1dc33708
Dharshan Prasath H <58679563+Dharshan2000@users.noreply.github.com>
2023-03-15 10:51:41 +0530

    Add files via upload

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] numbers = new int[5];
+            numbers[0]=4;
+            numbers[1]=5;
+            numbers[2]=6;
+            numbers[3]=7;
+            numbers[4]=8;
+            */
+            /*for(int i=0; i<numbers.Length; i++)
+            {
+                numbers[i]=Console.ReadLine();
+            }*/
+            string[] names = new string[] { "Dharshan", "Indra", "Divyashree", "Dinesh" };
+            /*for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i] + " ");
+            }
+            Console.ReadLine();
+            */
+            /*foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            */
+            string zig = "You can get what u want if u help enough other people"+"get what they want";
+            char[] charArray=zig.ToCharArray(); 
+            Array.Reverse(charArray);
+
+            foreach(char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
+        }
+    }
+}
