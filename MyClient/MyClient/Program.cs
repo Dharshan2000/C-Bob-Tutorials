c145729682d5425815e4b90983357e13fe57e3b7
Dharshan Prasath H <58679563+Dharshan2000@users.noreply.github.com>
2023-03-15 11:02:10 +0530

    Add files via upload

---
+++ MyClient/MyClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyCodelibrary;
+using System;
+namespace MyClient
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value= myScrape.ScrapeWebpage("https://msdn.microsoft.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
