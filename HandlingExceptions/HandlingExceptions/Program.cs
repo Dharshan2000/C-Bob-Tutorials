ce26acc2ac83a983e7e038d0296e56ab71eecab4
Dharshan Prasath H <58679563+Dharshan2000@users.noreply.github.com>
2023-03-15 18:24:26 +0530

    Add files via upload

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                string content = File.ReadAllText(@"C:\Users\Dharshan\source\repos\Testtextfil.txt");
+                Console.WriteLine(content);
+                Console.ReadLine();
+            }
+            catch(FileNotFoundException ex) 
+            {
+                Console.WriteLine("There is a problem");
+                Console.WriteLine("Make sure the filename is correct");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There is a problem");
+                Console.WriteLine("Make sure the directory is correct");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There is a problem");
+                Console.WriteLine(ex.Message);
+            }
+            finally { 
+                //Code to finalize
+                //Setting objects to null
+                //Closing database connections
+                   }
+            Console.ReadLine();
+        }
+    }
+
+}
