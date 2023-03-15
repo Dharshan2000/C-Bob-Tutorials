2ba1ab5d2f50cbc3c87eed7ad111d7018ba6b391
Dharshan Prasath H <58679563+Dharshan2000@users.noreply.github.com>
2023-03-15 11:08:50 +0530

    Add files via upload

---
+++ UnderstandingScope/UnderstandingScope/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UnderstandingScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
